bad2b4311fac6a469e5297b8eda94ddcf80e736d
Rémi Becheras <rbecheras@sirap.fr>
2015-12-15 12:32:40 +0100

    implement modularized directive from existing source

--- angular-focus-on-keydown.js
+++ angular-focus-on-keydown.js
@@ -6,13 +6,47 @@
  *
  */
 
-'use strict';
-
 (function(){
+'use strict';
+  
+    angular.module('focusOnKeydown').directive('focusOnKeydown', function($window) {
+      function isFocus(element){
+        return element[0] === document.activeElement;
+      }
+      function validateSymbols(evt, symbols){
+        var normalKey;
+        var hasCtrl, hasShift;
+        var isValid;
+        angular.forEach(symbols,function(symbol){
+          if (angular.lowercase(symbol) === angular.lowercase('CTRL')) {
+            hasCtrl = true;
+          } else if (angular.lowercase(symbol) === angular.lowercase('SHIFT')) {
+            hasShift = true;
+          } else normalKey = symbol;
+        });
+        // init 'isValid' to 'true' then search if it should be set to 'false'
+        isValid = true;
+        if (hasCtrl && !evt.ctrlKey) isValid = false;
+        if (hasShift && !evt.shiftKey) isValid = false;
+        if (evt.keyCode !== normalKey.charCodeAt(0)) isValid = false;
 
-  /**
-   * @TODO
-   * Write your bower component code here
-   */
+        return isValid;
+      }
+
+      return {
+        restrict: 'A',
+        link: function(scope, elem, attrs) {
+          $window.onkeydown = function(e){
+            console.log(e,attrs.focusOnKeydown);
+            if (validateSymbols(e, attrs.focusOnKeydown.split('+'))) {
+              if (!isFocus(elem)) {
+                e.preventDefault();
+                elem[0].focus();
+              }
+            }
+          };
+        }
+      };
+    });
 
 })();
