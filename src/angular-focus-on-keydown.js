b2eff897e5029258810ca480442dc8033d740d88
Rémi Becheras <rbecheras@sirap.fr>
2015-12-18 21:39:35 +0100

    move source to src

---
+++ src/angular-focus-on-keydown.js
@@ -0,0 +1,51 @@
+/**
+ * angular-focus-on-keydown.js
+ *
+ * Released under MIT License.
+ * Copyright (c) 2015 Rémi Becheras All rights reserved
+ *
+ */
+
+(function(){
+'use strict';
+
+    angular.module('focusOnKeydown',[]).directive('focusOnKeydown', function($window) {
+      function isFocus(element){
+        return element[0] === document.activeElement;
+      }
+      function validateSymbols(evt, symbols){
+        var normalKey;
+        var hasCtrl, hasShift;
+        var isValid;
+        angular.forEach(symbols,function(symbol){
+          if (angular.lowercase(symbol) === angular.lowercase('CTRL')) {
+            hasCtrl = true;
+          } else if (angular.lowercase(symbol) === angular.lowercase('SHIFT')) {
+            hasShift = true;
+          } else normalKey = symbol;
+        });
+        // init 'isValid' to 'true' then search if it should be set to 'false'
+        isValid = true;
+        if (hasCtrl && !evt.ctrlKey) isValid = false;
+        if (hasShift && !evt.shiftKey) isValid = false;
+        if (evt.keyCode !== normalKey.charCodeAt(0)) isValid = false;
+
+        return isValid;
+      }
+
+      return {
+        restrict: 'A',
+        link: function(scope, elem, attrs) {
+          $window.onkeydown = function(e){
+            if (validateSymbols(e, attrs.focusOnKeydown.split('+'))) {
+              if (!isFocus(elem)) {
+                e.preventDefault();
+                elem[0].focus();
+              }
+            }
+          };
+        }
+      };
+    });
+
+})();
